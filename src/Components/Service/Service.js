8070c13d1ef6adf2cc4b172963d4bb3ecb8b3f66
rifatjahan823 <rifatjahan823@gmail.com>
2022-05-30 03:52:36 +0600

    add divider

---
+++ src/Components/Service/Service.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Service.css'
+
+const Service = () => {
+    return (
+<div>
+<div className='d-flex align-items-center pt-5'>
+        <div style={{borderTop:"1px dashed #644F9C",width:"32%"}} className=' '></div>
+        <h3 className='px-5'style={{fontSize:"30px",color:'#644F9C'}}> AVAILABLE FOR EVERYONE</h3>
+        <div style={{borderTop:"1px dashed #644F9C",width:"32%"}} className=''></div>
+      </div>
+        <div className='container'>
+        <div className='service'>
+            <h5 className='text-center'>Here will be a text</h5>
+            <div className='row'>
+                <div className='col-lg-4 col-md-6 col-12'>
+                    
+                </div>
+                <div className='col-lg-4 col-md-6 col-12'>
+
+                </div>
+                <div className='col-lg-4 col-md-6 col-12'>
+
+                </div>
+            </div>
+        </div>
+     </div>
+</div>
+    );
+};
+
+export default Service;
