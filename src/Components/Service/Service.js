ac65162d847944b901ad6a933dbaa2bb5ba49d37
rifatjahan823 <rifatjahan823@gmail.com>
2022-05-30 05:06:04 +0600

    complete banner

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import './Service.css'
+import './Service.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBullhorn,faPaperPlane,faUser} from '@fortawesome/free-solid-svg-icons';
+
 
 const Service = () => {
     return (
@@ -10,17 +13,29 @@ const Service = () => {
         <div style={{borderTop:"1px dashed #644F9C",width:"32%"}} className=''></div>
       </div>
         <div className='container'>
-        <div className='service'>
+        <div className='service '>
             <h5 className='text-center'>Here will be a text</h5>
-            <div className='row'>
-                <div className='col-lg-4 col-md-6 col-12'>
-                    
+            <div className='row g-4 align-items-center justify-content-center mt-5 '>
+                <div className='col-lg-4 col-md-6 col-12  '>
+                <div className=''>
+                <FontAwesomeIcon className='icon' icon={faBullhorn} />
+                <h4 className='my-4'>text 1</h4>
+                <button className='btn'>Hello 1</button>
+                </div>
                 </div>
                 <div className='col-lg-4 col-md-6 col-12'>
-
+                <div>
+                <FontAwesomeIcon className='icon' icon={faPaperPlane} />
+                <h4 className='my-4'>text 2</h4>
+                <button className='btn'>Hello 1</button>
+                </div>
                 </div>
                 <div className='col-lg-4 col-md-6 col-12'>
-
+                <div className=''>
+                <FontAwesomeIcon className='icon' icon={faUser} />
+                <h4 className='my-4'> text 3</h4>
+                <button className='btn'>Hello 1</button>
+                </div>
                 </div>
             </div>
         </div>
