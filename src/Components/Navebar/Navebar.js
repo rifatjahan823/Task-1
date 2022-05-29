2c334ba4a2876d4e173005b4eb9307cc654e09cd
rifatjahan823 <rifatjahan823@gmail.com>
2022-05-30 02:49:42 +0600

    add main menu

--- src/Components/Navebar/Navebar.js
+++ src/Components/Navebar/Navebar.js
@@ -15,20 +15,21 @@ const Navebar = () => {
             </Container>
             </Navbar>
             </div>
-            <div>
+        {/* -------------main-menu-----------     */}
+            <div className='main-menu'>
             <Navbar collapseOnSelect expand="lg" sticky="top"className='bg'>
             <Container> 
-            <Navbar.Brand href="#home">
-          
-             </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
-             <Nav className="mx-auto">
+             <Nav className="me-auto">
              <Nav.Link href="#home">Home</Nav.Link>
                <Nav.Link href="#home" to="/blog">Blog</Nav.Link>
                <Nav.Link href="#home" to="/portfolio">My Portfolio</Nav.Link>
-
                <Nav.Link href="#home" to="/dashboard">DashBoard</Nav.Link>
+             </Nav>
+             <Nav className="ms-auto" style={{backgroundColor:"white",borderRadius:"40px",padding:"5px 30px",boxShadow:'0px 3px 2px -1px'}}>
+               <Nav.Link href="#home" style={{color:'black',fontSize:"18px"}} className='me-3'>Login</Nav.Link>
+               <Nav.Link href="#home" style={{backgroundColor:'#FF6F00',color:'white',width:"120px",height:'40px',borderRadius:'40px'}} >SIGN UP</Nav.Link>
              </Nav>
             </Navbar.Collapse>
           </Container>
